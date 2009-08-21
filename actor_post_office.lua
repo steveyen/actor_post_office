10f34df7cc222551b41a62f0a09e7cfcea6a1cfe
Steve Yen <steve.yen@gmail.com>
2009-08-21 08:45:28 -0700

    actor_socket and fixes

--- actor_post_office.lua
+++ actor_post_office.lua
@@ -16,6 +16,7 @@ local last_addr = 0
 
 local map_addr_to_coro = {} -- table, key'ed by addr.
 local map_coro_to_addr = {} -- table, key'ed by coro.
+local map_coro_to_data = {} -- table, key'ed by coro, for user data.
 
 local envelopes = {}
 
@@ -23,9 +24,9 @@ local envelopes = {}
 
 local main_todos = {} -- array of funcs/closures, to be run on main thread.
 
-local function run_main_todos()
+local function run_main_todos(force)
   -- Check first if we're the main thread.
-  if coroutine.running() == nil then
+  if (coroutine.running() == nil) or force then
     local todo = nil
     repeat
       todo = table.remove(main_todos, 1)
@@ -54,6 +55,7 @@ local function unregister(addr)
   if coro then
     map_addr_to_coro[addr] = nil
     map_coro_to_addr[coro] = nil
+    map_coro_to_data[coro] = nil
   end
 end
 
@@ -68,16 +70,50 @@ local function register(coro)
   return curr_addr
 end
 
+local function is_registered(addr)
+  return map_addr_to_coro[addr] ~= nil
+end
+
+local function coroutine_address(coro)
+  if coro then
+    return map_coro_to_addr[coro]
+  end
+  return nil
+end
+
+local function self_address()
+  return coroutine_address(coroutine.running())
+end
+
 ----------------------------------------
+
+local function user_data()
+  local c = coroutine.running()
+  local d = map_coro_to_data[c]
+  if not d then
+    d = {}
+    map_coro_to_data[c] = d
+  end
+  return d
+end
+
+----------------------------------------
+
+local function resume(coro, ...)
+  if coro and coroutine.status(coro) ~= 'dead' then
+    local ok = coroutine.resume(coro, ...)
+    if not ok then
+      print(debug.traceback(coro))
+    end
+    return ok
+  end
+  return false
+end
 
 local function deliver_envelope(envelope)
   -- Must be invoked on main thread.
   if envelope then
-    local coro = map_addr_to_coro[envelope.dest_addr]
-    if coro and coroutine.status(coro) ~= 'dead' then
-      coroutine.resume(coro, unpack(envelope.msg))
-    end
-    return true
+    return resume(map_addr_to_coro[envelope.dest_addr], unpack(envelope.msg))
   end
   return false
 end
@@ -89,9 +125,9 @@ local function step()
   return deliver_envelope(table.remove(envelopes, 1))
 end
 
-local function loop_until_empty()
+local function loop_until_empty(force)
   -- Check first if we're the main thread.
-  if coroutine.running() == nil then
+  if (coroutine.running() == nil) or force then
     local go = true
     while go do
       go = step()
@@ -107,14 +143,27 @@ end
 
 ----------------------------------------
 
+-- Asynchronous send of a msg table.
+--
 local function send_msg(dest_addr, msg)
   table.insert(envelopes, { dest_addr = dest_addr, msg = msg })
-
-  loop_until_empty()
 end
 
+-- Asynchronous send of variable args as a message.
+--
+local function send_later(dest_addr, ...)
+  send_msg(dest_addr, arg)
+end
+
+-- Asynchronous send of variable args as a message.
+--
+-- Unlike send_later(), a send() might opportunistically,
+-- process the message immediately before returning.
+--
 local function send(dest_addr, ...)
   send_msg(dest_addr, arg)
+
+  loop_until_empty()
 end
 
 local function recv()
@@ -127,7 +176,7 @@ end
 
 ----------------------------------------
 
-local function spawn(f, ...)
+local function spawn_with(spawner, f, ...)
   local child_coro = nil
   local child_addr = nil
   local child_arg = arg
@@ -136,12 +185,12 @@ local function spawn(f, ...)
                       unregister(child_addr)
                     end
 
-  child_coro = coroutine.create(child_fun)
+  child_coro = spawner(child_fun)
   child_addr = register(child_coro)
 
   table.insert(main_todos,
     function()
-      coroutine.resume(child_coro)
+      resume(child_coro)
     end)
 
   run_main_todos()
@@ -149,18 +198,27 @@ local function spawn(f, ...)
   return child_addr
 end
 
+local function spawn(f, ...)
+  return spawn_with(coroutine.create, f, ...)
+end
+
 ----------------------------------------
 
-local actor_post_office = {
-  spawn = spawn,
-  recv  = recv,
-  send  = send,
-  step  = step,
-  loop             = loop,
-  loop_until_empty = loop_until_empty
+return {
+  recv       = recv,
+  send       = send,
+  send_later = send_later,
+  step       = step,
+  spawn      = spawn,
+  spawn_with = spawn_with,
+  user_data  = user_data,
+  register   = register,
+  unregister = unregister,
+  is_registered     = is_registered,
+  coroutine_address = coroutine_address,
+  self_address      = self_address,
+  loop_until_empty  = loop_until_empty
 }
-
-return actor_post_office
 
 end
 
